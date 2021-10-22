138f149399408cd8aa16200df986ede411f8b80b
Thiago de Almeida Simões <thiago.simoes_desenv@hotmail.com>
2021-10-22 17:48:50 -0300

    Jump implementation 2

--- Assets/Scripts/Megabot.cs
+++ Assets/Scripts/Megabot.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class Megabot : MonoBehaviour
 {
     public float speed = 7f;
+    public float jumpForce = 12f;
+    public float collisionRadius = 0.25f;
+    public Transform foot;
+    public LayerMask layerGround;
 
+    private bool jump = false;
+    private bool onFloor = false;
     private Vector2 direction = Vector2.zero;
     private Rigidbody2D rb;
 
@@ -18,12 +24,23 @@ public class Megabot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Jump") && onFloor)
+        {
+            jump = true;
+        }
+
+        GroundCheck();
     }
 
     void FixedUpdate()
     {
         Move(Input.GetAxisRaw("Horizontal"));
+
+        if (jump)
+        {
+            jump = false;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 
     void Move(float move)
@@ -34,11 +51,22 @@ public class Megabot : MonoBehaviour
         if(move < 0)
             direction = Vector2.left;
 
-        rb.velocity = new Vector2(direction.x * speed, direction.y);
+        rb.velocity = new Vector2(direction.x * speed, rb.velocity.y);
 
         if(move == 0)
-            rb.velocity = new Vector2(0, direction.y);
+            rb.velocity = new Vector2(0, rb.velocity.y);
 
         transform.right = direction;
+    }
+
+    void GroundCheck()
+    {
+        onFloor = Physics2D.OverlapCircle(foot.position, collisionRadius, layerGround);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere((Vector2)foot.position, collisionRadius);
     }
 }
